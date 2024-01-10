a745355a076f6a7debda8893ec1dba4e07161610
jurie24102 <jurie24102@gmail.com>
2024-01-10 18:48:35 +0200

    title added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/Dieoutjiemuisic.png" />
-        <title>Dieoutjiemuisic</title>
+        <title>Play ~ Dieoutjiemuisic</title>
       </Head>
       <TopNav />
       <Component {...pageProps} />
