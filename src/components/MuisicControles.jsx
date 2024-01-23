be7bd2203351410ae0d4cb55c58aa60f41275e16
jurie24102 <jurie24102@gmail.com>
2024-01-23 17:36:09 +0200

    songs

---
+++ src/components/MuisicControles.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from 'next/image';
+import { Box } from '@mui/material';
+import { ColorLinearProgress } from './MusicPlayer'; // assuming this is exported from MusicPlayer
+
+const MusicControls = ({ isPlaying, togglePlay, skipBackward, skipForward, currentSong, progress, songDuration, handleProgressBarClick }) => {
+  return (
+    <>
+      <Box mt={2}>{currentSong.title}</Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '70%',
+          mt: 3,
+          alignItems: 'center',
+          maxWidth: 300,
+        }}
+      >
+        <Image src="/skip-previous.svg" alt="Previous" width={20} height={20} onClick={skipBackward}/>
+        <Image src={!isPlaying ? '/play.svg' : 'pause.svg'} alt="Play Icon" width={40} height={40} onClick={togglePlay}/>
+        <Image src="/skip-next.svg" alt="next" width={20} height={20} onClick={skipForward}/>
+      </Box>
+      <Box sx={{color: 'white'}} mt={4}>{currentSong.title}</Box>
+      <ColorLinearProgress
+        onClick={handleProgressBarClick}
+        sx={{ height: 10, width: {xs: '90%', md: '40%'}, borderRadius: 5, border: '1px solid orange', mt: 4}}
+        variant="determinate"
+        value={(progress / songDuration) * 100}
+      />
+    </>
+  );
+};
+
+export default MusicControls;
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,8 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button,
-  Container, Box, Typography
+  Container, Box, Typography, LinearProgress, styled
  } from '@mui/material';
+ import MusicControls from './MuisicControles';
+
+const ColorLinearProgress = styled(LinearProgress)({
+  color: '#ffff00', // Change this to the color you want
+  '& .MuiLinearProgress-barColorPrimary': {
+    backgroundColor: '#ffff00', // Change this to the color you want
+  },
+});
 
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -10,7 +18,8 @@ const MusicPlayer = () => {
   const [progress, setProgress] = useState(0);
   const audioRef = useRef(null);
   const [openDialog, setOpenDialog] = useState(false);
-const [selectedSong, setSelectedSong] = useState(null);
+  const [selectedSong, setSelectedSong] = useState(null);
+  const [songDuration, setSongDuration] = useState(0);
 
 const handleDownload = (url, title) => {
   setSelectedSong({ url, title });
@@ -27,87 +36,105 @@ const confirmDownload = () => {
   setOpenDialog(false);
 };
 
+const handleProgressBarClick = (event) => {
+  const progressBar = event.currentTarget;
+  const boundingRect = progressBar.getBoundingClientRect();
+  const clickedPositionInPx = event.clientX - boundingRect.left;
+  const widthOfProgressBarInPx = boundingRect.width;
+  const clickedPositionAsPercentage = clickedPositionInPx / widthOfProgressBarInPx;
+  const newCurrentTime = clickedPositionAsPercentage * songDuration;
+
+  if (audioRef.current) {
+    audioRef.current.currentTime = newCurrentTime;
+    setProgress(newCurrentTime);
+  }
+};
+
   const songs = [
+    // {
+    //   url: '/songs/Kani-slaapie.mp3',
+    //   title: 'Kani slaapie - Dieoutjiemusic',
+    //   cover: '/Dieoutjiemuisic.png',
+    // },
     {
-      url: '/Kani-slaapie.mp3',
-      title: 'Kani slaapie - Dieoutjiemusic',
+      url: '/songs/Myne.mp3',
+      title: 'Myne - Dieoutjiemusic',
       cover: '/Dieoutjiemuisic.png',
     },
-    // {
-    //   url: '/Myne.mp3',
-    //   title: 'Myne - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/Rugsteekers.mp3',
-    //   title: 'Rugsteekers - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
     {
-      url: '/Bozza.mp3',
+      url: '/songs/Rugsteekers.mp3',
+      title: 'Rugsteekers - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/Bozza.mp3',
       title: 'Bozza - Dieoutjiemusic',
       cover: '/Dieoutjiemuisic.png',
     },
     {
-      url: '/Blomme.mp3',
+      url: '/songs/Blomme.mp3',
       title: 'Blomme - Dieoutjiemusic',
       cover: '/Dieoutjiemuisic.png',
     },
+    // {
+    //   url: '/songs/DangerousGirl.mp3',
+    //   title: 'Dangerous Girl - Dieoutjiemusic',
+    //   cover: '/Dieoutjiemuisic.png',
+    // },
     {
-      url: '/DangerousGirl.mp3',
-      title: 'Dangerous Girl - Dieoutjiemusic',
-      cover: '/Dieoutjiemuisic.png',
-    },
-    {
-      url: '/HOE-LYKIT.mp3',
+      url: '/songs/HOE-LYKIT.mp3',
       title: 'HOE LYKIT - Dieoutjiemusic',
       cover: '/Dieoutjiemuisic.png',
     },
-    // {
-    //   url: '/Hoelank-nog.mp3',
-    //   title: 'Hoelank nog - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/KEN-JO-WAARDE.mp3',
-    //   title: 'KEN JO WAARDE - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/kroon.mp3',
-    //   title: 'kroon - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/LEWE-CHANGE.mp3',
-    //   title: 'LEWE CHANGE - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/Loyalty.mp3',
-    //   title: 'Loyalty - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/Vir-My,My.mp3',
-    //   title: 'Vir My,My - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
-    // {
-    //   url: '/Waardes.mp3',
-    //   title: 'Waardes - Dieoutjiemusic',
-    //   cover: '/Dieoutjiemuisic.png',
-    // },
+    {
+      url: '/songs/Hoelank-nog.mp3',
+      title: 'Hoelank nog - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/KEN-JO-WAARDE.mp3',
+      title: 'KEN JO WAARDE - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/kroon.mp3',
+      title: 'kroon - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/LEWE-CHANGE.mp3',
+      title: 'LEWE CHANGE - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/Loyalty.mp3',
+      title: 'Loyalty - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/Vir-My,My.mp3',
+      title: 'Vir My,My - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
+    {
+      url: '/songs/Waardes.mp3',
+      title: 'Waardes - Dieoutjiemusic',
+      cover: '/Dieoutjiemuisic.png',
+    },
     // Add more song objects here
   ];
 
   const playSong = (songIndex, startTime = 0, playImmediately = true) => {
   const audioElement = new Audio(songs[songIndex].url);
   audioElement.currentTime = startTime;
+  audioElement.onloadedmetadata = () => {
+    setSongDuration(audioElement.duration); // Set song duration
+  };
   if (playImmediately) {
     audioElement.play();
     setIsPlaying(true);
   }
+  
   setCurrentSongIndex(songIndex);
   audioRef.current = audioElement;
   setProgress(0); // Reset progress when changing the song
@@ -141,6 +168,8 @@ const confirmDownload = () => {
       audioRef.current.play();
       setIsPlaying(true);
     }
+    setProgress(0); // Reset progress
+    setSongDuration(0); // Reset song duration
   };
 
   const skipBackward = () => {
@@ -150,16 +179,26 @@ const confirmDownload = () => {
     if (isPlaying) {
       audioRef.current.pause();
     }
-    setProgress(0); // Reset progress when skipping backward
     playSong(currentSongIndex - 1, 0);
   } else {
     if (isPlaying) {
       audioRef.current.pause();
     }
-    setProgress(0); // Reset progress when skipping to the same song
     playSong(currentSongIndex, 0);
   }
+  setProgress(0); // Reset progress
+  setSongDuration(0); // Reset song duration
 };
+
+useEffect(() => {
+    const interval = setInterval(() => {
+      if (isPlaying && audioRef.current) {
+        setProgress(audioRef.current.currentTime);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isPlaying]);
 
 useEffect(() => {
     const currentSongElement = document.getElementById(`song-${currentSongIndex}`);
@@ -194,6 +233,7 @@ useEffect(() => {
         justifyContent: 'center',
         alignItems: 'center',
         height: '90vh', // Adjust the height as needed
+        
       }}
     >
       <Box mt={5}>
@@ -208,12 +248,20 @@ useEffect(() => {
           width: '70%',
           mt: 3,
           alignItems: 'center',
+          maxWidth: 300,
         }}
       >
         <Image src="/skip-previous.svg" alt="Previous" width={20} height={20} onClick={skipBackward}/>
         <Image src={!isPlaying ? '/play.svg' : 'pause.svg'} alt="Play Icon" width={40} height={40} onClick={togglePlay}/>
         <Image src="/skip-next.svg" alt="next" width={20} height={20} onClick={skipForward}/>
       </Box>
+      <Box sx={{color: 'white'}} mt={4}>{songs[currentSongIndex].title}</Box>
+      <ColorLinearProgress
+        onClick={handleProgressBarClick}
+        sx={{ height: 10, width: {xs: '90%', md: '40%'}, borderRadius: 5, border: '1px solid orange', mt: 4}}
+        variant="determinate"
+        value={(progress / songDuration) * 100}
+      />
       <Box sx={{ width: '100%', mt: 3, height: '400px', overflowY: 'auto' }}>
       {songs.map((song, index) => (
         <>
